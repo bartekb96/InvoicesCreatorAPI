78b1cfe6a16461e3dc7cd2a8915b9e346e4590b6
Bartek <bartekbednarek51@gmail.com>
2021-06-21 21:09:42 +0200

    fixing namespaces

--- Test/Tests/ApplicationLayerTests/UserTests.cs
+++ Test/Tests/ApplicationLayerTests/UserTests.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Test.Models;
+using InvoicesCreator.Test.Models;
 using Xunit;
 
-namespace Test.Tests.ApplicationLayerTests
+namespace InvoicesCreator.Test.Tests.ApplicationLayerTests
 {
     public class UserTests
     {
         [Fact]
         public void ShouldCreateNewUser()
         {
-            User ExpectedUser = new User
-            {
-                UserName = "Bartek",
-                Email = "testowy@mail.com",
-                Password = "A94A8FE5CCB19BA61C4C0873D391E987982FBBD3",
-                UserType = 1,
-                SellerID = null
-            };
+            //User ExpectedUser = new User
+            //{
+            //    UserName = "Bartek",
+            //    Email = "testowy@mail.com",
+            //    Password = "A94A8FE5CCB19BA61C4C0873D391E987982FBBD3",
+            //    UserType = 1,
+            //    SellerID = null
+            //};
 
-            User ActualUser = 
+            //User ActualUser = 
         }
     }
 }
